05605f718b013531edf505f1413f360ab0056a43
Alex Bowyer <alex.bowyer@gmail.com>
2020-06-03 18:05:49 +0100

    Working on host ready screen - prepareHostReadyScreen method in index.php. Unfinished.

--- index.php
+++ index.php
@@ -58,6 +58,13 @@ function getState()
     return $res[0]['state'];
 }
 
+function countPlayers()
+{
+    $res = runSQL("SELECT COUNT(player_name) as n FROM players;");
+    return $res[0]['n'];
+}
+
+
 function getCurrentQuestion()
 {
     $res = runSQL("SELECT question_number FROM state WHERE n=1;");
@@ -108,7 +115,41 @@ function stringArrayToJSArray($arr)
     return '["' . implode('", "', $arr) . '"]';
 }
 
-function get_data_and_prep_render() {
+function debug($name, $obj)
+{
+    highlight_string("<?php\n\$$name =\n" . var_export($obj, true) . ";\n?>");
+}
+
+function questionDataToJSArrayString($q)
+{
+    $n = $q['question_number'];
+    $t = $q['question_text'];
+    $oA = $q['option_a'];
+    $oB = $q['option_b'];
+    $oC = $q['option_c'];
+    $oD = $q['option_d'];
+    $a1 = $q['first'];
+    $a2 = $q['second'];
+    $a3 = $q['third'];
+    $a4 = $q['fourth'];
+
+    return "[$n,\"$t\",\"$oA\",\"$oB\",\"$oC\",\"$oD\",\"$a1\",\"$a2\",\"$a3\",\"$a4\"]";
+}
+
+function questionsDataToJSArray()
+{
+    $res = runSQL("SELECT * FROM questions;");
+    $str = "";
+
+    foreach ($res as $q) {
+        $str .= questionDataToJSArrayString($q) . ", \n";
+    }
+    $str = "[".substr($str, 0, -3)."]";
+    return $str;
+}
+
+function get_data_and_prep_render()
+{
     global $action, $detail, $state, $question_number, $number_of_players, $player_list, $page_contents, $render_html;
     $state = getState();
     // state can be:
@@ -138,6 +179,7 @@ function get_data_and_prep_render() {
     // put data into the page's javascript for client-side use
     $page_contents = str_replace("<POLL_URL>", '/poll/status', $page_contents);
     $page_contents = str_replace("<BUTTONS_CLICKED_URL>", '/post/answer', $page_contents);
+    $page_contents = str_replace("<START_ROUND_URL>", '/post/start', $page_contents);
     $page_contents = str_replace("<ACTION>", $action, $page_contents);
     $page_contents = str_replace("<STATE>", $state, $page_contents);
     $page_contents = str_replace("<DETAIL>", $detail, $page_contents);
@@ -152,10 +194,12 @@ function get_data_and_prep_render() {
         $page_contents = str_replace("<NUMBER_OF_PLAYERS>", '0', $page_contents);
     }
     if ($player_list) {
-        $page_contents = str_replace("<PLAYER_LIST>", stringArrayToJSArray($player_list), $page_contents);
+        $page_contents = str_replace('"<PLAYER_LIST>"', stringArrayToJSArray($player_list), $page_contents);
     } else {
-        $page_contents = str_replace("<PLAYER_LIST>", '[]', $page_contents);
+        $page_contents = str_replace('"<PLAYER_LIST>"', '[]', $page_contents);
     }
+    $questionsDataAsJSString = questionsDataToJSArray();
+    $page_contents = str_replace('"<QUESTION_ARRAY>"', $questionsDataAsJSString, $page_contents);
 
     $render_html = true;
 }
@@ -183,8 +227,12 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         $question_number = $_POST['question_number'];
         $answer = $_POST['answer'];
         $query = "INSERT IGNORE INTO times(question_number,event_time,answer,player_name) VALUES(?, NOW() ,?, ?);";
-        echo $query;
         runSQLPrepared($query, $question_number, $answer, $player_name);
+    } else if ($target = "start") {
+        header('HTTP/1.1 200 OK');
+        $number_of_players = countPlayers();
+        $query = 'UPDATE state SET state="ready", question_number=1, number_of_players = ? WHERE n=1;';
+        runSQLPrepared($query, $number_of_players);
     } else {
         header('HTTP/1.1 500 Internal Server Error');
         echo 'unknown target ' + $target;
@@ -238,7 +286,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         }
     } else if (count($url_parts) == 2) {
         $action = $url_parts[1];
-        if ($action=="host") {
+        if ($action == "host") {
             get_data_and_prep_render();
             // host is active
             if ($render_html) {
@@ -249,7 +297,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             echo "<h1>ERROR</h1><p>You have entered an invalid web address. Try: <span style='font-family:Arial;color:green;;'>http://fastestfingerfirst.alexbowyer.com/play/yourname</span></p>";
         }
     } else {
-      echo "<h1>ERROR</h1><p>You have entered an invalid web address. Try: <span style='font-family:Arial;color:green;;'>http://fastestfingerfirst.alexbowyer.com/play/yourname</span></p>";
+        echo "<h1>ERROR</h1><p>You have entered an invalid web address. Try: <span style='font-family:Arial;color:green;;'>http://fastestfingerfirst.alexbowyer.com/play/yourname</span></p>";
     }
 }
 disconnect($conn);
